48ef9b670061bac63e9ec88f478645e4f30a4542
hafeezifaruqi <hafeezi.faruqi@nusantara.technology>
2024-10-28 16:21:42 +0700

    feat: Update proto files from external repository
    This commit updates the proto files in the local project directory by cloning the faruqi-protos repository and copying the updated proto files. The update process checks if the proto repository already exists locally and either updates it or clones it if it doesn't exist. The old proto files are removed from the local project directory before copying the updated files.
    
    Closes #123

--- update_proto.sh
+++ update_proto.sh
@@ -3,18 +3,28 @@
 # Directory where the proto files will be saved locally
 LOCAL_PROTO_DIR="external/proto"
 
-# Remove the old proto files
+# Temporary directory for the proto repository
+TMP_PROTO_REPO="/tmp/faruqi-protos"
+
+# Check if the proto repo already exists locally
+if [ -d "$TMP_PROTO_REPO" ]; then
+    # If it exists, update the repository
+    echo "Updating existing proto repository..."
+    git -C $TMP_PROTO_REPO pull
+else
+    # Clone the repository if it does not exist
+    echo "Cloning proto repository..."
+    git clone --depth 1 https://github.com/faruqii/faruqi-protos.git $TMP_PROTO_REPO
+fi
+
+# Remove old proto files from local project directory
 rm -rf $LOCAL_PROTO_DIR
 
-# Clone the proto repository 
-git clone --depth 1 https://github.com/faruqii/faruqi-protos.git /tmp/faruqi-protos
-
-# Copy the proto files to the local project directory
+# Create local proto directory if it doesn't exist
 mkdir -p $LOCAL_PROTO_DIR
-cp -r /tmp/faruqi-protos/proto/products/*.proto $LOCAL_PROTO_DIR
-cp -r /tmp/faruqi-protos/proto/users/*.proto $LOCAL_PROTO_DIR
 
-# Clean up the temporary clone
-rm -rf /tmp/faruqi-protos
+# Copy updated proto files to the local project directory
+cp -r $TMP_PROTO_REPO/proto/products/*.proto $LOCAL_PROTO_DIR
+cp -r $TMP_PROTO_REPO/proto/users/*.proto $LOCAL_PROTO_DIR
 
 echo "Proto files updated in $LOCAL_PROTO_DIR"
